8e75b43714132c35b3d1e95f7a35b59fcf9d9a28
jmaeat <jmaeatmon@icloud.com>
2023-01-07 13:32:18 -0800

    config: restore settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,24 @@
 import { defineConfig } from "astro/config"
 
+import image from "@astrojs/image"
 import mdx from "@astrojs/mdx"
-import netlify from "@astrojs/netlify/edge-functions"
 import react from "@astrojs/react"
 import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [mdx(), react(), tailwind()],
-	adapter: netlify(),
-	output: "server",
+	integrations: [
+		mdx(),
+		react(),
+		tailwind(),
+		image({
+			serviceEntryPoint: "@astrojs/image/sharp",
+		}),
+	],
 	site: "https://eatmon.co",
+	vite: {
+		ssr: {
+			noExternal: ["smartypants"],
+		},
+	},
 })
