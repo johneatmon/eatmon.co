22f0b26a3d187f061e27b662ea72f54f59c10f06
jmaeat <jmaeatmon@icloud.com>
2023-01-07 12:27:23 -0800

    config: SSR, Netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,23 @@
 import { defineConfig } from "astro/config"
 
-// https://astro.build/config
-import react from "@astrojs/react"
-
-// https://astro.build/config
-import tailwind from "@astrojs/tailwind"
-
-// https://astro.build/config
-import mdx from "@astrojs/mdx"
-
-// https://astro.build/config
 import image from "@astrojs/image"
+import mdx from "@astrojs/mdx"
+import netlify from "@astrojs/netlify/edge-functions"
+import react from "@astrojs/react"
+import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
-		react(),
-		tailwind(),
-		mdx(),
 		image({
 			serviceEntryPoint: "@astrojs/image/sharp",
 		}),
+		mdx(),
+		react(),
+		tailwind(),
 	],
+	adapter: netlify(),
+	output: "sever",
 	site: "https://eatmon.co",
 	vite: {
 		ssr: {
