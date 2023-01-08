b22700539f68ae920767cc484729d91dbd9ca645
jmaeat <jmaeatmon@icloud.com>
2023-01-07 20:15:23 -0800

    feat: theme toggle

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,7 @@ const defaultTheme = require("tailwindcss/defaultTheme")
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-	darkMode: "media",
+	darkMode: "class",
 	future: {
 		hoverOnlyWhenSupported: true,
 	},
