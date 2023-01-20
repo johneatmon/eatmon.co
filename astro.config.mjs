8911e810067ddf6ff4dd9b89b472240b3d61fd70
jmaeat <jmaeatmon@icloud.com>
2023-01-19 18:29:13 -0800

    config: enable vercel + adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,9 @@ import { defineConfig } from "astro/config"
 
 import image from "@astrojs/image"
 import mdx from "@astrojs/mdx"
-import netlify from "@astrojs/netlify/functions"
 import react from "@astrojs/react"
 import tailwind from "@astrojs/tailwind"
+import vercel from "@astrojs/vercel/serverless"
 
 // https://astro.build/config
 export default defineConfig({
@@ -16,5 +16,5 @@ export default defineConfig({
 		},
 	},
 	output: "server",
-	adapter: netlify(),
+	adapter: vercel(),
 })
