da9f4bb622db50f7af6c5dc76301c8848fff304e
jmaeat <jmaeatmon@icloud.com>
2023-01-19 18:30:09 -0800

    chore: clean up a bit (doesn't work in prod)

--- src/pages/api/glimpse.ts
+++ src/pages/api/glimpse.ts
@@ -6,19 +6,16 @@ const headers = {
 }
 
 export const post: APIRoute = async ({ request }): Promise<Response> => {
-	if (request.headers.get("Content-Type") === "application/json") {
-		const { url } = (await request.json()) as { url?: string }
+	const { url } = (await request.json()) as { url?: string }
 
-		if (!url) {
-			return new Response(JSON.stringify({}), { status: 400, headers })
-		}
-
-		try {
-			const data = await glimpse(url)
-			return new Response(JSON.stringify(data), { status: 200, headers })
-		} catch {
-			return new Response(JSON.stringify({}), { status: 500, headers })
-		}
+	if (!url) {
+		return new Response(JSON.stringify({}), { status: 400, headers })
 	}
-	return new Response(null, { status: 400 })
+
+	try {
+		const data = await glimpse(url)
+		return new Response(JSON.stringify(data), { status: 200, headers })
+	} catch {
+		return new Response(JSON.stringify({}), { status: 500, headers })
+	}
 }
