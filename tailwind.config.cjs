d9469334225efa24a1e7991034f655009acb54f3
jmaeat <jmaeatmon@icloud.com>
2023-01-19 19:45:11 -0800

    feat: fancy button

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -11,7 +11,11 @@ module.exports = {
 		extend: {
 			animation: {
 				comeInOut: "comeInOut 700ms forwards",
+				disco: "disco 4s linear infinite",
 				spinSparkles: "spin 1000ms linear",
+			},
+			backgroundImage: {
+				"gradient-conic": "conic-gradient(var(--tw-gradient-stops))",
 			},
 			fontFamily: {
 				display: ["Hatton", ...defaultTheme.fontFamily.sans],
@@ -22,6 +26,10 @@ module.exports = {
 				comeInOut: {
 					"0%, 100%": { transform: "scale(0)" },
 					"50%": { transform: "scale(1)" },
+				},
+				disco: {
+					"0%": { transform: "translateY(-50%) rotate(0deg)" },
+					"100%": { transform: "translateY(-50%) rotate(360deg)" },
 				},
 				spin: {
 					"0%": { transform: "rotate(0deg)" },
