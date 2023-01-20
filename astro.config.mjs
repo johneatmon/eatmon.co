14237cd73f8999b12cd11eb716af9b2a8851bb0b
jmaeat <jmaeatmon@icloud.com>
2023-01-19 19:44:56 -0800

    build/feat: prevent widows with a remark plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,15 @@ import react from "@astrojs/react"
 import tailwind from "@astrojs/tailwind"
 import vercel from "@astrojs/vercel/serverless"
 
+import { remarkWidont } from "./remark-widont.mjs"
+
 // https://astro.build/config
 export default defineConfig({
 	integrations: [mdx(), react(), tailwind(), image()],
+	markdown: {
+		remarkPlugins: [remarkWidont],
+		extendDefaultPlugins: true,
+	},
 	site: "https://eatmon.co",
 	vite: {
 		ssr: {
