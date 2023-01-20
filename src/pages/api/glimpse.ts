2314f65c8c70d8dc81a4038cf557091acb2dcb6e
jmaeat <jmaeatmon@icloud.com>
2023-01-19 18:43:54 -0800

    feat: let's make some lawgs

--- src/pages/api/glimpse.ts
+++ src/pages/api/glimpse.ts
@@ -5,8 +5,10 @@ const headers = {
 	"content-type": "application/json",
 }
 
-export const post: APIRoute = async ({ request }): Promise<Response> => {
+export const post: APIRoute = async function get({ request }): Promise<Response> {
 	const { url } = (await request.json()) as { url?: string }
+
+	console.log(url)
 
 	if (!url) {
 		return new Response(JSON.stringify({}), { status: 400, headers })
@@ -14,6 +16,7 @@ export const post: APIRoute = async ({ request }): Promise<Response> => {
 
 	try {
 		const data = await glimpse(url)
+		console.log(data)
 		return new Response(JSON.stringify(data), { status: 200, headers })
 	} catch {
 		return new Response(JSON.stringify({}), { status: 500, headers })
