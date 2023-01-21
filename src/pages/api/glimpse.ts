f7fe7ad21753c6db4fd272aab96f0bc9da97420e
jmaeat <jmaeatmon@icloud.com>
2023-01-20 21:28:44 -0800

    fix: had the if/else backwards lol

--- src/pages/api/glimpse.ts
+++ src/pages/api/glimpse.ts
@@ -1,3 +1,4 @@
+import type { PreviewResponse } from "@beskar-labs/glimpse/server"
 import glimpse from "@beskar-labs/glimpse/server"
 import type { APIRoute } from "astro"
 
@@ -8,18 +9,15 @@ const headers = {
 export const post: APIRoute = async ({ request }): Promise<Response> => {
 	const { url } = (await request.json()) as { url?: string }
 
-	console.log(url)
-
 	if (!url) {
 		return new Response(JSON.stringify({}), { status: 400, headers })
 	}
 
-	const data = await glimpse(url)
-	console.log(data)
+	const data: PreviewResponse = await glimpse(url)
 
 	if (!data) {
-		return new Response(JSON.stringify(data), { status: 200, headers })
-	} else {
 		return new Response(JSON.stringify({}), { status: 500, headers })
+	} else {
+		return new Response(JSON.stringify(data), { status: 200, headers })
 	}
 }
