17cd555f82134c8cbdbf6e034a76591d652928b9
jmaeat <jmaeatmon@icloud.com>
2023-01-22 10:02:14 -0800

    fix: try/catch

--- src/pages/api/glimpse.ts
+++ src/pages/api/glimpse.ts
@@ -13,11 +13,11 @@ export const post: APIRoute = async ({ request }): Promise<Response> => {
 		return new Response(JSON.stringify({}), { status: 400, headers })
 	}
 
-	const data: PreviewResponse = await glimpse(url)
+	try {
+		const data: PreviewResponse = await glimpse(url)
 
-	if (!data) {
-		return new Response(JSON.stringify({}), { status: 500, headers })
-	} else {
 		return new Response(JSON.stringify(data), { status: 200, headers })
+	} catch (err) {
+		return new Response(JSON.stringify(err), { status: 500, headers })
 	}
 }
