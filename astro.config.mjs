b72692367f638ee47820ad27fe00822882c7dabc
jmaeat <jmaeatmon@icloud.com>
2023-01-22 09:59:28 -0800

    config: try Vercel Edge, disable `@astrojs/image`

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,17 @@
 import { defineConfig } from "astro/config"
 
-import image from "@astrojs/image"
+// import image from "@astrojs/image"
 import mdx from "@astrojs/mdx"
 import react from "@astrojs/react"
 import tailwind from "@astrojs/tailwind"
-import vercel from "@astrojs/vercel/serverless"
+import vercel from "@astrojs/vercel/edge"
 
 import { remarkReadingTime } from "./remark-reading-time.mjs"
 import { remarkWidont } from "./remark-widont.mjs"
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [mdx(), react(), tailwind(), image()],
+	integrations: [mdx(), react(), tailwind()],
 	markdown: {
 		remarkPlugins: [remarkWidont, remarkReadingTime],
 		extendDefaultPlugins: true,
