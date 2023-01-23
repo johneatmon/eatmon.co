7e357d6f1c0dff1c1e2252d1b1895df2ebf77db6
jmaeat <jmaeatmon@icloud.com>
2023-01-22 18:59:57 -0800

    config: add Radix UI toast animations

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -13,6 +13,12 @@ module.exports = {
 				comeInOut: "comeInOut 700ms forwards",
 				disco: "disco 4s linear infinite",
 				spinSparkles: "spin 1000ms linear",
+				// Toast
+				"toast-hide": "toast-hide 100ms ease-in forwards",
+				"toast-slide-in-right": "toast-slide-in-right 150ms cubic-bezier(0.16, 1, 0.3, 1)",
+				"toast-slide-in-bottom": "toast-slide-in-bottom 150ms cubic-bezier(0.16, 1, 0.3, 1)",
+				"toast-swipe-out-x": "toast-swipe-out-x 100ms ease-out forwards",
+				"toast-swipe-out-y": "toast-swipe-out-y 100ms ease-out forwards",
 			},
 			backgroundImage: {
 				"gradient-conic": "conic-gradient(var(--tw-gradient-stops))",
@@ -34,6 +40,31 @@ module.exports = {
 				spin: {
 					"0%": { transform: "rotate(0deg)" },
 					"100%": { transform: "rotate(180deg)" },
+				},
+				// Toast
+				"toast-hide": {
+					"0%": { opacity: 1 },
+					"100%": { opacity: 0 },
+				},
+				"toast-slide-in-right": {
+					"0%": { transform: `translateX(calc(100% + 1rem))` },
+					"100%": { transform: "translateX(0)" },
+				},
+				"toast-slide-in-bottom": {
+					"0%": { transform: `translateY(calc(100% + 1rem))` },
+					"100%": { transform: "translateY(0)" },
+				},
+				"toast-swipe-out-x": {
+					"0%": { transform: "translateX(var(--radix-toast-swipe-end-x))" },
+					"100%": {
+						transform: `translateX(calc(100% + 1rem))`,
+					},
+				},
+				"toast-swipe-out-y": {
+					"0%": { transform: "translateY(var(--radix-toast-swipe-end-y))" },
+					"100%": {
+						transform: `translateY(calc(100% + 1rem))`,
+					},
 				},
 			},
 			lineHeight: {
