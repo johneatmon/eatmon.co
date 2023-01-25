5da6f1ddc91d944396166140251792cb22174fd9
jmaeat <jmaeatmon@icloud.com>
2023-01-24 19:33:29 -0800

    config: enable drafts

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,6 +15,7 @@ export default defineConfig({
 	markdown: {
 		remarkPlugins: [remarkWidont, remarkReadingTime],
 		extendDefaultPlugins: true,
+		drafts: true,
 	},
 	site: "https://eatmon.co",
 	vite: {
