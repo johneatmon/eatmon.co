52a2005cab65beeaae20d64c0f2c4350f5927d17
jmaeat <jmaeatmon@icloud.com>
2023-01-24 19:37:34 -0800

    feat: theme toggle

---
+++ src/components/ThemeToggle.tsx
@@ -0,0 +1,36 @@
+import { MoonIcon, SunIcon } from "@heroicons/react/20/solid"
+import type { FC } from "react"
+import { useEffect, useState } from "react"
+
+const ThemeToggle: FC = () => {
+	const [theme, setTheme] = useState(localStorage.getItem("theme") ?? "dark")
+
+	const handleClick = () => {
+		setTheme(theme === "light" ? "dark" : "light")
+	}
+
+	useEffect(() => {
+		if (theme === "dark") {
+			// document.documentElement.classList.add("dark")
+			document.documentElement.setAttribute("data-mode", "dark")
+		} else {
+			// document.documentElement.classList.remove("dark")
+			document.documentElement.removeAttribute("data-mode")
+		}
+		localStorage.setItem("theme", theme)
+	}, [theme])
+
+	return (
+		<button
+			type="button"
+			aria-label="Toggle dark mode"
+			onClick={handleClick}
+			className="group relative grid place-items-center py-4 px-[18px] -mr-[18px] rounded-full"
+		>
+			<SunIcon className="w-5 aspect-square text-gray-11 transition-colors group-hover:text-gray-12 dark:hidden" />
+			<MoonIcon className="hidden w-5 aspect-square text-gray-11 transition-colors group-hover:text-gray-12 dark:block" />
+		</button>
+	)
+}
+
+export default ThemeToggle
