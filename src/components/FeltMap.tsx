0481065d3e32b4e7ff1b801dec9c0256c043e3ea
jmaeat <jmaeatmon@icloud.com>
2023-01-29 10:49:28 -0800

    chore: img/perf improvements

---
+++ src/components/FeltMap.tsx
@@ -0,0 +1,12 @@
+const FeltMap = () => {
+	return (
+		<iframe
+			className="w-full border-none aspect-square rounded-lg"
+			title="My Felt Map of Tacoma, WA"
+			src="https://felt.com/embed/map/My-home-9AgWsx7e5SjCLf9Cghe0RG8A?lat=47.287319&lon=-122.391078&zoom=11.400331391880338"
+			loading="lazy"
+		></iframe>
+	)
+}
+
+export default FeltMap
