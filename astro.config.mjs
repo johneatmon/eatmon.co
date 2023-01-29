71608c67af9fc0299165c951e9cc9284bf0b5614
jmaeat <jmaeatmon@icloud.com>
2023-01-29 12:05:44 -0800

    config: enable `astro-compress`

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import mdx from "@astrojs/mdx"
 import react from "@astrojs/react"
 import tailwind from "@astrojs/tailwind"
 import vercel from "@astrojs/vercel/serverless"
+import compress from "astro-compress"
 
 import { remarkReadingTime } from "./remark-reading-time.mjs"
 import { remarkWidont } from "./remark-widont.mjs"
@@ -12,6 +13,11 @@ import { remarkWidont } from "./remark-widont.mjs"
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
+		compress({
+			html: false,
+			img: false,
+			svg: false,
+		}),
 		image({
 			serviceEntryPoint: "@astrojs/image/sharp",
 		}),
