0e82477c50fb05dc6e5958d15e28589cb2c205e3
jmaeat <jmaeatmon@icloud.com>
2023-03-12 10:32:38 -0700

    config: remove unused integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from "astro/config"
-import { FontaineTransform } from "fontaine"
 
 import image from "@astrojs/image"
 import mdx from "@astrojs/mdx"
 import react from "@astrojs/react"
 import tailwind from "@astrojs/tailwind"
 import vercel from "@astrojs/vercel/serverless"
-import autoImport from "astro-auto-import"
 import compress from "astro-compress"
 import robots from "astro-robots-txt"
 
@@ -16,13 +14,6 @@ import { remarkWidont } from "./remark-plugins/remark-widont.mjs"
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
-		autoImport({
-			imports: [
-				{
-					"./src/lib/smartypants.ts": ["smarty"],
-				},
-			],
-		}),
 		compress({
 			html: false,
 			img: false,
@@ -56,12 +47,6 @@ export default defineConfig({
 	},
 	site: "https://eatmon.co/",
 	vite: {
-		plugins: [
-			FontaineTransform.vite({
-				fallbacks: ["Arial"],
-				resolvePath: (id) => new URL(`./public${id}`, import.meta.url),
-			}),
-		],
 		ssr: {
 			noExternal: [/^@radix-ui\/*/, "react-wrap-balancer", "smartypants"],
 		},
