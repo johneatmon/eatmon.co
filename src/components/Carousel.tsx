e4c083507f6f7c6f9e4773d4205ede5cb5c6c422
John Eatmon <34781354+johneatmon@users.noreply.github.com>
2023-03-19 15:11:12 -0700

    fix: use `<LazyImage />`, use TS aliases

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -24,13 +24,13 @@ const Carousel = ({ children }: { children: ReactNode }) => {
 			<div className="blaze-container">
 				<div className="mb-4 flex justify-end gap-x-3">
 					<button
-						className="blaze-prev | grid aspect-square h-11 place-items-center rounded-full border border-gray-300 bg-white transition hover:bg-gray-300/70 dark:border-gray-800 dark:bg-black dark:hover:bg-gray-900"
+						className="blaze-prev | grid aspect-square h-11 place-items-center rounded-full border border-gray-300 bg-white !font-sans leading-none transition hover:bg-gray-300/70 dark:border-gray-800 dark:bg-black dark:hover:bg-gray-900"
 						aria-label="Go to previous slide"
 					>
 						&larr;
 					</button>
 					<button
-						className="blaze-next | grid aspect-square h-11 place-items-center rounded-full border border-gray-300 bg-white transition hover:bg-gray-300/70 dark:border-gray-800 dark:bg-black dark:hover:bg-gray-900"
+						className="blaze-next | grid aspect-square h-11 place-items-center rounded-full border border-gray-300 bg-white !font-sans leading-none transition hover:bg-gray-300/70 dark:border-gray-800 dark:bg-black dark:hover:bg-gray-900"
 						aria-label="Go to next slide"
 					>
 						&rarr;
