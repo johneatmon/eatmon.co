855828c0d0155dda8c7328865de4bff0439c5c83
John Eatmon <34781354+johneatmon@users.noreply.github.com>
2023-12-04 20:41:17 -0800

    add redirects for socials, old content

--- next.config.mjs
+++ next.config.mjs
@@ -28,6 +28,50 @@ const nextConfig = {
 			},
 		],
 	},
+	async redirects() {
+		return [
+			{
+				source: '/github',
+				destination: 'https://github.com/johneatmon',
+				permanent: true,
+			},
+			{
+				source: '/x',
+				destination: 'https://twitter.com/johneatmon_',
+				permanent: true,
+			},
+			{
+				source: '/cv',
+				destination: 'https://read.cv/johneatmon',
+				permanent: true,
+			},
+			{
+				source: '/work/:slug*',
+				destination: 'https://read.cv/johneatmon',
+				permanent: false,
+			},
+			{
+				source: '/project/:slug*',
+				destination: 'https://read.cv/johneatmon/npsVXnwnPLc03Cfs6863',
+				permanent: false,
+			},
+			{
+				source: '/about',
+				destination: '/',
+				permanent: false,
+			},
+			{
+				source: '/about/colophon',
+				destination: '/colophon',
+				permanent: true,
+			},
+			{
+				source: '/about/tools',
+				destination: '/blog/things-i-use',
+				permanent: true,
+			},
+		];
+	},
 	webpack: (config) => {
 		config.infrastructureLogging = {
 			level: 'error',
