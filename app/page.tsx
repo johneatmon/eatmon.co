ba3d7f118fed94ed731c1aef1f9ca084a8d3598e
John Eatmon <34781354+johneatmon@users.noreply.github.com>
2023-12-04 20:51:59 -0800

    clean up

--- app/page.tsx
+++ app/page.tsx
@@ -7,14 +7,6 @@ export default async function Home() {
 	return (
 		<main className='mx-auto flex max-w-2xl flex-col gap-16 px-4 pb-24 pt-[128px]'>
 			<Header />
-			{/* <div>
-				<Shine
-					className='font-serif text-5xl font-bold tracking-tight text-[#0F0F0F]'
-					lightColor='#f7f7f7'
-				>
-					John Eatmon
-				</Shine>
-			</div> */}
 			<AboutSection className='mt-16' />
 			<WorkSection />
 			<BlogSection count={3} />
