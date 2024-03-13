ee34aeb2f4f1bfa1741f2f46b12c31c4d044f1ea
John Eatmon <34781354+johneatmon@users.noreply.github.com>
2024-03-12 17:40:26 -0700

    Lisan al Gaib

--- components/header.tsx
+++ components/header.tsx
@@ -4,11 +4,11 @@ const Header = () => {
 	return (
 		<header>
 			<BlurImage
-				src='https://eatmon.co/cdn-cgi/imagedelivery/le40TwFDWUdIvXckEp8FBw/ebab5e6e-2096-4954-3a37-b83bbea1b300/square'
-				className='mb-4 aspect-square w-10 rounded-full transition-all dark:grayscale'
-				alt='Grayscale headshot of John Eatmon'
-				width={40}
-				height={40}
+				src='https://eatmon.co/cdn-cgi/imagedelivery/le40TwFDWUdIvXckEp8FBw/499582c7-d0cd-49dc-8cf1-249ad7bef400/square'
+				className='mb-4 aspect-square w-20 rounded-full transition-all dark:grayscale'
+				alt='Grayscale headshot of John Eatmon with spicy blue Dune eyes and a filt-plug in his nose.'
+				width={80}
+				height={80}
 				loading='eager'
 			/>
 			<small className='flex max-w-max flex-col font-sans text-base'>
